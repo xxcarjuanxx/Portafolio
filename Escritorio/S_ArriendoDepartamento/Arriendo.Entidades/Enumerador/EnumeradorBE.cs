86ae88aa60ce36f8dde749b79eb487dc5482bb3a
jorge-bv <bravov.jo@gmail.com>
2020-10-06 19:03:00 -0300

    Cambios

---
+++ Escritorio/S_ArriendoDepartamento/Arriendo.Entidades/Enumerador/EnumeradorBE.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arriendo.Entidades.Enumerador
+{
+    public enum TipoCheck { 
+    CheckIn = 0,
+    CheckOut = 1
+    }
+}
