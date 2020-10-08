2a934a52e5e00a230b1e9e48cab722cd2e541d69
jorge-bv <bravov.jo@gmail.com>
2020-10-07 23:21:22 -0300

    cambios checklist

---
+++ Escritorio/S_ArriendoDepartamento/Arriendo.Negocio/CheckListBL.cs
@@ -0,0 +1,30 @@
+﻿using Arriendo.Datos;
+using Arriendo.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arriendo.Negocio
+{
+    public class CheckListBL
+    {
+        CheckListDA ochecklistDA;
+        public CheckListBL()
+        {
+            ochecklistDA = new CheckListDA();
+        }
+        public List<CheckListBE> ListarChecklist()
+        {
+            try
+            {
+                return ochecklistDA.Listarchecklist();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
