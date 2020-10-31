6ceced62ca0b6329eb3ba7b9ee26654cc4186ad7
xxcarjuanxx <juan.colonia.ceda@gmail.com>
2020-10-31 02:29:19 -0300

    parte 2 reserva menu

--- Escritorio/S_ArriendoDepartamento/Arriendo.Presentacion/MainWindow.xaml.cs
+++ Escritorio/S_ArriendoDepartamento/Arriendo.Presentacion/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -279,5 +280,7 @@ namespace Arriendo.Presentacion
                 this.WindowState = WindowState.Maximized;
             }     
         }
+     
+        
     }
 }
