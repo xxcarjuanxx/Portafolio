8b7e3377508808646961d850a14f8c64c60331e3
xxcarjuanxx <juan.colonia.ceda@gmail.com>
2020-11-10 23:52:47 -0300

    proceso de pdf

--- Escritorio/S_ArriendoDepartamento/Arriendo.Test/Test.cs
+++ Escritorio/S_ArriendoDepartamento/Arriendo.Test/Test.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Office.Interop.Word;
+using System.Diagnostics;
 
 namespace Arriendo.Test
 {
@@ -20,8 +23,9 @@ namespace Arriendo.Test
         {
             try
             {
+                CrearPDF();
                 //ListaComunaId();
-                LoginUsuario();
+                //LoginUsuario();
                 Console.ReadKey();
             }
             catch (Exception ex)
@@ -62,6 +66,53 @@ namespace Arriendo.Test
                 Console.WriteLine("Nombre : " + item.NombreUsuario);
                 Console.WriteLine("-------------------------------------------");
             }
+        }
+
+        private static void CrearPDF() {
+
+            try
+            {
+                Microsoft.Office.Interop.Word.Application word = new Microsoft.Office.Interop.Word.Application();
+
+                object oMissing = System.Reflection.Missing.Value;
+                word.Visible = false;
+                word.ScreenUpdating = false;
+
+                FileInfo wordFile = new FileInfo("C:\\xampp\\htdocs\\prueba.docx");
+                Object filename = (Object)wordFile.FullName;
+
+                Microsoft.Office.Interop.Word.Document doc = word.Documents.Open(ref filename, ref oMissing,
+                    ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                    ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                    ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+                doc.Activate();
+
+                object outputFileName = wordFile.FullName.Replace(".docx", ".pdf");
+                object fileFormat = WdSaveFormat.wdFormatPDF;
+
+                // Save document into PDF Format
+                doc.SaveAs(ref outputFileName,
+                    ref fileFormat, ref oMissing, ref oMissing,
+                    ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                    ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                    ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+
+                object saveChanges = WdSaveOptions.wdDoNotSaveChanges;
+                ((_Document)doc).Close(ref saveChanges, ref oMissing, ref oMissing);
+                doc = null;
+
+
+                ((_Application)word).Quit(ref oMissing, ref oMissing, ref oMissing);
+                word = null;
+                Process.Start(outputFileName.ToString());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            
+
         }
     }
 }
